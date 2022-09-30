20e58334fb0e370365781fdfa25f7605cfd41ea8
Ben <b.bax23@hotmail.co.uk>
2022-09-30 12:16:39 +0100

    add Comments component, render a  list of comments for each single review

---
+++ src/components/Comments.jsx
@@ -0,0 +1,33 @@
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+
+const Comments = ({review_id}) => {
+
+    const [comments, setComments] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://be-games-example-api.herokuapp.com/api/reviews/${review_id}/comments`).then(({data}) => {
+            setComments(data.comments)
+        })
+    }, [review_id])
+
+    return (
+        <>
+        <ul>
+            <h2>Comments</h2>
+            {comments.map((comment) => {
+                return (
+                    <li key={comment.comment_id}>
+                        <h3>{comment.author} on {comment.created_at}</h3>
+                        <p>{comment.body}</p>
+                        <p>Votes: {comment.votes}</p>
+                    </li>
+                )
+            })}
+        </ul>
+        </>
+    )
+}
+
+export default Comments
